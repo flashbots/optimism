bf37eff9227c8c8341ea4a521eca285ce3331b3e
Bhakiyaraj Kalimuthu <bhakiya.kalimuthu@gmail.com>
2022-12-07 21:13:46 +0100

    add server timeout

--- proxyd/server.go
+++ proxyd/server.go
@@ -170,8 +170,11 @@ func (s *Server) RPCListenAndServe(host string, port int) error {
 	})
 	addr := fmt.Sprintf("%s:%d", host, port)
 	s.rpcServer = &http.Server{
-		Handler: instrumentedHdlr(c.Handler(hdlr)),
-		Addr:    addr,
+		Handler:      instrumentedHdlr(c.Handler(hdlr)),
+		Addr:         addr,
+		ReadTimeout:  2 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  63 * time.Second,
 	}
 	log.Info("starting HTTP server", "addr", addr)
 	s.srvMu.Unlock()
